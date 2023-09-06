e489ef31ee036cc6503da4c903c9a51b0e321635
chill duck <johannesagustinlr@gmail.com>
2023-09-06 09:17:43 +0700

    journal

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -7,8 +7,15 @@ class EstateProperty(models.Model):
     _inherit = "estate.property"
 
 
-    def action_sold(self):
+    def sold(self):
         res = super().sold()
-
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
+        invoice_vals = {
+            'move_type': 'out_invoice',
+            'partner_id': self.buyer_id.id,
+            'journal_id': journal.id,  # company comes from the journal
+            'invoice_line_ids': [],
+        }
+        moves = self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice_vals)
 
         return res
